ee00e326490098a4095bdb406ba4255b0e272726
Edgar Jair Beltran Sanchez <edgar.beltran@coppel.com>
2024-10-28 12:50:46 -0700

    Firestore configuration

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm # type: ignore
+from wtforms.fields import StringField, PasswordField, SubmitField # type: ignore
+from wtforms.validators import DataRequired # type: ignore
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()], render_kw={'class': 'custom-input'})
+    username = StringField('Username', validators=[DataRequired()], render_kw={'class': 'custom-input', "autocomplete": "off"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={'class': 'custom-input'})
     submit = SubmitField('Send', render_kw={'class': 'custom-button'})
 
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import unittest
-from flask import request, make_response, redirect, render_template, session, flash, url_for
-from flask_login import login_required, current_user
+from flask import (  # type: ignore
+    request,
+    make_response,
+    redirect,
+    render_template,
+    session,
+    flash,
+    url_for,
+)
+from flask_login import login_required, current_user  # type: ignore
 
 from app import create_app
 from app.forms import TodoForm, DeleteTodoForm
@@ -11,61 +19,61 @@ app = create_app()
 
 @app.cli.command()
 def test():
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner().run(tests)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html', error=error)
+    return render_template("404.html", error=error)
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    return render_template('500.html', error=error)
+    return render_template("500.html", error=error)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     user_ip = request.remote_addr
-    response = make_response(redirect('/hello'))
-    session['user_ip'] = user_ip
+    response = make_response(redirect("/hello"))
+    session["user_ip"] = user_ip
     return response
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route("/hello", methods=["GET", "POST"])
 @login_required
 def hello():
-    user_ip = session.get('user_ip')
+    user_ip = session.get("user_ip")
     username = current_user.id
     todo_form = TodoForm()
-    delete_form = DeleteTodoForm
+    delete_form = DeleteTodoForm()
 
     context = {
-        'user_ip': user_ip,
-        'todos': get_todos(user_id=username),
-        'username': username,
-        'todo_form': todo_form,
-        'delete_todo_form': delete_form,
+        "user_ip": user_ip,
+        "todos": get_todos(user_id=username),
+        "username": username,
+        "todo_form": todo_form,
+        "delete_todo_form": delete_form,
     }
 
     if todo_form.validate_on_submit():
         put_todo(user_id=username, description=todo_form.description.data)
 
-        flash('Your task was created successfully!')
+        flash("Your task was created successfully!")
 
-        return redirect(url_for('hello'))
+        return redirect(url_for("hello"))
 
-    return render_template('hello.html', **context)
+    return render_template("hello.html", **context)
 
 
-@app.route('/todos/delete/<todo_id>', methods=['POST'])
+@app.route("/todos/delete/<todo_id>", methods=["POST"])
 def delete(todo_id):
     user_id = current_user.id
     delete_todo(user_id=user_id, todo_id=todo_id)
 
-    return redirect(url_for('hello'))
+    return redirect(url_for("hello"))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
