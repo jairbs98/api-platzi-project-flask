c1f9234d995dc84d27d16a27a562c131a09c6804
Edgar Jair Beltran Sanchez <edgar.beltran@coppel.com>
2024-10-31 16:21:56 -0700

    Migrating from Firestore to PostgreSQL using SQLAlchemy

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean # type: ignore
-from sqlalchemy.ext.declarative import declarative_base # type: ignore
-from sqlalchemy.orm import sessionmaker, relationship # type: ignore
+from sqlalchemy import create_engine  # type: ignore
+from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
 
 DATABASE_URL = "postgresql://postgres:admin@bd-todo-flask-container:5432/todo"
 
@@ -8,6 +8,7 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
