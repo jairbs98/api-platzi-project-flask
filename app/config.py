049fd589efeeff51e445fb453ba7dde559bb6e4d
Jair Victus <jairbs98@gmail.com>
2024-11-06 18:29:53 -0700

    Update models and config.py

--- app/config.py
+++ app/config.py
@@ -1,2 +1,5 @@
 class Config:
-    SECRET_KEY = 'SUPER SECRET'
+    SECRET_KEY = "SUPER SECRET"
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:admin@bd-todo-flask-container:5432/todo"
+    )
